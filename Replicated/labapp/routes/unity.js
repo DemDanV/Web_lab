adc7a32911eabecf7cdf7ded7bbdb7a62f31b319
DemDanV <dementiev201@gmail.com>
2023-05-12 11:51:40 +0500

    Unity page pug fixed

--- Replicated/labapp/routes/unity.js
+++ Replicated/labapp/routes/unity.js
@@ -14,3 +14,27 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
+
+$(window).load(function () {
+  //new WOW().init();
+  // initialize isotope
+  var $container = $('.portfolio_container');
+  $container.isotope({
+      filter: '*',
+  });
+
+  $('.portfolio_filter a').click(function () {
+      $('.portfolio_filter .active').removeClass('active');
+      $(this).addClass('active');
+
+      var selector = $(this).attr('data-filter');
+      $container.isotope({
+          filter: selector,
+          animationOptions: {
+              duration: 500,
+              animationEngine: "jquery"
+          }
+      });
+      return false;
+  });
+})
