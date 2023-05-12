04b56da6a1fa6c1edf601986900452b2e7b900c2
DemDanV <dementiev201@gmail.com>
2023-05-12 14:08:52 +0500

    Added DB

---
+++ Replicated/labapp/public/javascripts/highlight_cur_navmenu_tab.js
@@ -0,0 +1,36 @@
+$(window).load(function () {
+    var express = require('express');
+    var app = express();
+    console.log(app._router)
+    // // Получаем элемент <nav> и сохраняем его в переменную navmenu
+    // var navmenu = document.getElementsByClassName("first-level")[0];
+    // // Получаем все элементы <li> внутри элемента <nav> и сохраняем их в переменную navlist
+    // var navlist = navmenu.getElementsByTagName("li");
+    // // Получаем значение id элемента <main> и сохраняем его в переменную main_id
+    // var main_id = location.pathname.split('/')[7].split('.')[0];
+    // //alert(location.pathname.split('/')[7].split('.')[0]);
+    // // Создаем переменную menuelem, в которую будет сохранен элемент <li>, который будет активным на текущей странице
+    // var menuelem;
+    // switch (main_id) {
+    // case "index":
+    // menuelem = navlist[0];
+    // break;
+    // case "about":
+    // menuelem = navlist[1];
+    // break;
+    // case "unity":
+    // menuelem = navlist[2];
+    // break;
+    // case "android":
+    // menuelem = navlist[3];
+    // break;
+    // case "blog":
+    // menuelem = navlist[4];
+    // break;
+    // case "contact":
+    // menuelem = navlist[5];
+    // break;
+    // }
+    // // Добавляем класс "active" к элементу, сохраненному в переменной menuelem, чтобы он был подсвечен на странице
+    // menuelem.classList.add("active");
+});
